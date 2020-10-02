76e2d0ee7698ffb8b51c8496e6341d5d01baaecd
Patricia Arnedo <paaarnedo@gmail.com>
2020-10-02 08:32:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
+  array = nil
   data.each do |key, value|
     value.each do |k2, v2|
-      
-      
+      array << v2 
     end
   end
+  
+  array.uniq!
   
   
 end
